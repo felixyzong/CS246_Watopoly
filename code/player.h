adca425f5c068e3b08996c0353d09cb25a911a17
defhjoefjo <pipipi527@gmail.com>
2022-07-22 03:08:17 +0800

    Add basic functionality of player class

---
+++ code/player.h
@@ -0,0 +1,39 @@
+#ifndef WATOPOLY_PLAYER_H
+#define WATOPOLY_PLAYER_H
+
+
+#include <vector>
+
+class Player {
+  long money;
+  std::vector<Building *> property;
+  // ...
+  int cup; // The numebr of Roll Up the Rim Cup
+  int TimLineTurn = 0;
+  bool isInTimLine = false;
+  int pos = 0;
+  char name;
+  bool rolled = false;
+ public:
+  Player(long money, char name);
+  void addFund(int num); // To increase the money for a certain number
+  int getTotalWorth();   // get the total wealth
+  int rollDice(unsigned seed);        // get a random number
+  void move(int num);    // To move on the board for a certain number
+  void gainCup();        // Increase the cup by 1
+  int getTimTurn();      // Get the number of turn the player is stuck at time line
+  int incTimTurn();      // Increase the tim line turn
+  bool isInTim();
+  void buyProperty(Building *b);
+  int getPos();
+  void setPos(int pos); // differentiates "sent" to a place and "move" to a place
+  char getName();
+  bool isRolled();
+  void addProperty(Building *b);
+  void buyImprovement(Building *b);
+  void mortgage(Building *b);
+  void unMortgage(Building *b);
+};
+
+
+#endif
