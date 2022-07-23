72956b17c399068325012921c68f6af0c9738d92
Yokdy <109126557+Yokdy@users.noreply.github.com>
2022-07-23 00:33:03 -0400

    Delete enum.h

--- code/enum.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ENUM_H_
-#define _ENUM_H_
-
-enum class BuildingType { Academic, Residence, Gyms, Nonproperty };
-enum class BuildingName { AL, ML, ECH, PAS, HH, RCH, DWE, CPH, LHI, BMH, OPT, EV1, EV2, EV3, PHYS, B1, B2, EIT, ESC, C2, MC, DC, MKV，UWP, V1, REV, PAC, CIF, CollectOSAP, DCTimsLine, GoToTims, GooseNesting, Tuition, CoopFee, SLC, NeedlesHall}; 
-enum class MonopolyBlock { Arts1, Arts2, Eng, Health, Env, Sci1, Sci2, Math };
-
-#endif
